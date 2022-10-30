e58624e549aa55bb6204563abdbe2a29a514c338
HASSARI Ossama <o9hassar@enib.fr>
2022-10-30 23:29:18 +0100

    add: script exec tsunami bash

--- internal/cmd/ps.exec.go
+++ internal/cmd/ps.exec.go
@@ -82,14 +82,15 @@ func (p *Proc) RunScan(cArgs ...string) (string, error) {
 	}
 	log.Infof("%s", fmt.Sprintf(args))
 	//ps := exec.Command(p.getCMD(), args)
-	ps := exec.Command("java",
+	/*ps := exec.Command("java",
 		"-cp",
 		"\"/home/c-tsunami/tsunami/tsunami-main-0.0.15-SNAPSHOT-cli.jar:/home/c-tsunami/tsunami/plugins/*\"",
 		"com.google.tsunami.main.cli.TsunamiCli", //<-there is the prob
 		"--ip-v4-target=127.0.0.1",
 		"--scan-results-local-output-format=JSON",
 		"--scan-results-local-output-filename=/home/c-tsunami/tsunami/tsunami-output.json",
-	)
+	)*/
+	ps := exec.Command("./script-tsunami.sh")
 
 	var out, er bytes.Buffer
 
---
+++ script-tsunami.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp /home/c-tsunami/tsunami/tsunami-main-0.0.15-SNAPSHOT-cli.jar:/home/c-tsunami/tsunami/plugins/*\" com.google.tsunami.main.cli.TsunamiCli --ip-v4-target=127.0.0.1 --scan-results-local-output-format=JSON --scan-results-local-output-filename=/home/c-tsunami/tsunami/tsunami-output.json
