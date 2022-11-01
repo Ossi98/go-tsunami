272de31dfd00e2034d8900e616b276bfbbda8692
HASSARI Ossama <o9hassar@enib.fr>
2022-11-01 13:40:38 +0100

    test bash exec

--- internal/cmd/ps.exec.go
+++ internal/cmd/ps.exec.go
@@ -91,7 +91,7 @@ func (p *Proc) RunScan(cArgs ...string) (string, error) {
 		"--scan-results-local-output-filename=/home/c-tsunami/tsunami/tsunami-output.json",
 	)*/
 	//ps := exec.Command("./script-tsunami.sh")
-	ps := exec.Command("bash", "java -cp \"/home/c-tsunami/tsunami/tsunami-main-0.0.15-SNAPSHOT-cli.jar:/home/c-tsunami/tsunami/plugins/*\" com.google.tsunami.main.cli.TsunamiCli --ip-v4-target=127.0.0.1 --scan-results-local-output-format=JSON --scan-results-local-output-filename=/home/c-tsunami/tsunami/tsunami-output.json")
+	ps := exec.Command("java", "-cp \"/home/c-tsunami/tsunami/tsunami-main-0.0.15-SNAPSHOT-cli.jar:/home/c-tsunami/tsunami/plugins/*\" com.google.tsunami.main.cli.TsunamiCli --ip-v4-target=127.0.0.1 --scan-results-local-output-format=JSON --scan-results-local-output-filename=/home/c-tsunami/tsunami/tsunami-output.json")
 
 	var out, er bytes.Buffer
 
