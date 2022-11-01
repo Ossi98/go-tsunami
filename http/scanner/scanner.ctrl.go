3017233d16873f23176d39faafeadd8b8c5dfd67
HASSARI Ossama <o9hassar@enib.fr>
2022-11-01 17:49:25 +0100

    fix: open file json readonly

--- http/scanner/scanner.ctrl.go
+++ http/scanner/scanner.ctrl.go
@@ -81,8 +81,8 @@ func (s *Scanner) ReadScanFile(c *gin.Context) {
 	}
 
 	// Open our jsonFile
-	//jsonFile, err := os.Open(viper.GetString("cmd.tsunami.path") + uri.Id + ".json")
-	jsonFile, err := os.OpenFile(fmt.Sprintf("%s%s.json", s.viper.GetString("cmd.tsunami.path"), uri.Id), os.O_RDWR, 0444)
+	jsonFile, err := os.Open(fmt.Sprintf("%s%s.json", s.viper.GetString("cmd.tsunami.path"), uri.Id))
+	//jsonFile, err := os.OpenFile(fmt.Sprintf("%s%s.json", s.viper.GetString("cmd.tsunami.path"), uri.Id), os.O_RDWR, 0444)
 	// if we os.Open returns an error then handle it
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, map[string]any{
